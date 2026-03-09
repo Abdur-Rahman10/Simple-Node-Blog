70167bd7ecd360cb901897a4c88a4521647de673
ABDUR RAHMAN <abdurrahman.2003.1030@gmail.com>
2026-03-10 01:00:27 +0530

    Fixed port binding for Render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import postRoutes from './routes/posts.js'; // Import our new router file!
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
