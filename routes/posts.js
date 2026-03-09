48f24f1081f5f3fac59196ecb6c2a7f0c55a9d67
ABDUR RAHMAN <abdurrahman.2003.1030@gmail.com>
2026-03-10 01:09:24 +0530

    Added auto-folder creation for Multer

--- routes/posts.js
+++ routes/posts.js
@@ -7,6 +7,11 @@ import fs from 'fs';
 
 const router = express.Router();
 
+const uploadDir = 'public/uploads/';
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, 'public/uploads/'),
     filename: (req, file, cb) => cb(null, Date.now() + path.extname(file.originalname))
